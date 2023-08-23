0440696c6d513704a9f4e97abde3b56b8df8ebe9
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-23 14:24:20 -0400

    Fixed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    while(num < 0){
+        return "ERROR";
+    }
+    let word = "";
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+  return word;
+    
 };
 
 // Do not edit below this line
