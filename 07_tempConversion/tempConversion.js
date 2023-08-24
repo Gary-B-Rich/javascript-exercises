920a028bc53227752f78155d1b630a0d52a29491
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-24 14:48:11 -0400

    forgot to save

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
 const convertToCelsius = function(temp) {
   //convert temp to C
   let celcius = ((temp-32)*5/9);
+  //round to one decimal and return result
   return Math.round(celcius*10)/10;
 };
 
 const convertToFahrenheit = function(temp) {
   //convert temp to F
   let fahrenheit = ((temp*9/5)+32);
+  //round to one decimal and return result
   return Math.round(fahrenheit*10)/10;
 };
 
