cc7fed92f347c8ef95c87f9685340487ccebd4e8
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2023-08-24 11:46:26 -0400

    All working

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function(arr, remove) {
-    //add function
+
+
+
+// define function
+const removeFromArray = function(arr, ...remove) {
+    remove.forEach((remove) => {
+        const index = arr.indexOf(remove);
+        if (index > -1)
+          arr.splice(index, 1);
+      });
+      return arr;
 };
+
+//removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
